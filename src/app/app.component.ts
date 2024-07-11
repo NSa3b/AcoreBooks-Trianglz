c8e0c6cff744b0d85d2f0f5987f9cfd967f21f3b
Noha Saab <noha.flojics.technology>
2024-07-11 23:48:07 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule, InputTextModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AcoreBooks';
+  title:string = 'NOHA';
+  value : string | undefined = "noha.saab@hotmail.com";
 }
